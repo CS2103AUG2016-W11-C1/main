a55c15717b732a44b22b7e3f85fed268575f652a
cadmusthefounder <cadmusthefounder@gmail.com>
2016-09-19 21:12:46 +0800

    Merge mainwindow.fxml

--- src/linenux/Main.java
+++ src/linenux/Main.java
@@ -14,23 +14,22 @@ public class Main extends Application implements Stoppable {
 
     /** Version information of the program. */
     public static final String VERSION = "Linenux - Version 0.1";
-    
+
     private Gui gui;
 
-    
     @Override
     public void start(Stage primaryStage) throws Exception {
         gui = new Gui(new Logic(), VERSION);
         gui.start(primaryStage, this);
     }
-    
+
     @Override
     public void stop() throws Exception {
         super.stop();
         Platform.exit();
         System.exit(0);
     }
-    
+
     public static void main(String args) {
         launch(args);
     }
