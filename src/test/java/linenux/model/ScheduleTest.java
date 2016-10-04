54dcfe2cc6834ecaf1db38f5f15ab86c976074f7
Yihang Ho <hoyihang5@gmail.com>
2016-10-05 01:02:15 +0800

    Implement Schedule#search

---
+++ src/test/java/linenux/model/ScheduleTest.java
@@ -0,0 +1,48 @@
+package linenux.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by yihangho on 10/5/16.
+ */
+public class ScheduleTest {
+    private Schedule schedule;
+
+    @Before
+    public void setupSchedule() {
+        this.schedule = new Schedule();
+    }
+
+    @Test
+    public void testAddTask() {
+        int beforeSize = this.schedule.getTaskList().size();
+        this.schedule.addTask(new Task("bla"));
+        int afterSize = this.schedule.getTaskList().size();
+
+        assertEquals(beforeSize + 1, afterSize);
+    }
+
+    @Test
+    public void testSearch() {
+        String[] keywords = {"hello", "WoRlD"};
+        Task match1 = new Task("Say Hello");
+        Task match2 = new Task("Around the world");
+        Task mismatch = new Task("meh");
+
+        this.schedule.addTask(match1);
+        this.schedule.addTask(mismatch);
+        this.schedule.addTask(match2);
+
+        ArrayList<Task> tasks = this.schedule.search(keywords);
+
+        assertEquals(2, tasks.size());
+        assertTrue(tasks.indexOf(match1) != -1);
+        assertTrue(tasks.indexOf(match2) != -1);
+    }
+}
