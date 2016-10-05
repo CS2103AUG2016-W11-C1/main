a25a4a71a22c174b89677cfcb0a29538dba8dd56
Yihang Ho <hoyihang5@gmail.com>
2016-10-05 11:38:24 +0800

    Implement list with search

--- src/main/java/linenux/command/ListCommand.java
+++ src/main/java/linenux/command/ListCommand.java
@@ -6,12 +6,14 @@ import linenux.model.Task;
 import linenux.util.TasksListUtil;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by yihangho on 10/4/16.
  */
 public class ListCommand implements Command {
-    private static final String TASK_PATTERN = "(?i)^list$";
+    private static final String TASK_PATTERN = "(?i)^list( (?<keywords>.*))?$";
 
     private Schedule schedule;
 
@@ -26,7 +28,19 @@ public class ListCommand implements Command {
 
     @Override
     public CommandResult execute(String userInput) {
-        return makeResult(this.schedule.getTaskList());
+        Matcher matcher = Pattern.compile(TASK_PATTERN).matcher(userInput);
+
+        if (matcher.matches()) {
+            String keywords = matcher.group("keywords");
+
+            if (keywords != null) {
+                return makeResult(this.schedule.search(keywords.split("//s+")));
+            } else {
+                return makeResult(this.schedule.getTaskList());
+            }
+        }
+
+        return null;
     }
 
     private CommandResult makeResult(ArrayList<Task> tasks) {
