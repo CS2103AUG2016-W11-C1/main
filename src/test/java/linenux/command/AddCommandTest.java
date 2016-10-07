17476ac9422ad2dfce21ddc7049d7dac552f4ad2
Yihang Ho <hoyihang5@gmail.com>
2016-10-07 16:37:53 +0800

    Handle the case when no argument is given for the add command

--- src/test/java/linenux/command/AddCommandTest.java
+++ src/test/java/linenux/command/AddCommandTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static linenux.helpers.Assert.assertChangeBy;
+import static linenux.helpers.Assert.assertNoChange;
 import static org.junit.Assert.*;
 
 /**
@@ -26,11 +27,16 @@ public class AddCommandTest {
     }
 
     /**
-     * Test that respondTo detects various versions of the commands.
+     * Test that respondTo detects various versions of the commands. It should return true even if
+     * the format of the arguments are invalid.
      */
     @Test
     public void testRespondToAddTaskCommand() {
+        assertTrue(this.addCommand.respondTo("add"));
         assertTrue(this.addCommand.respondTo("add CS2103T Tutorial"));
+        assertTrue(this.addCommand.respondTo("add CS2103T Tutorial st/2016-01-01"));
+        assertTrue(this.addCommand.respondTo("add CS2103T Tutorial et/2016-01-01"));
+        assertTrue(this.addCommand.respondTo("add CS2103T Tutorial st/2016-01-01 et/2016-01-01"));
     }
 
     /**
@@ -77,5 +83,25 @@ public class AddCommandTest {
     public void testExecuteAddCommandResult() {
         CommandResult result = this.addCommand.execute("add CS2103T Tutorial");
         assertEquals("Added CS2103T Tutorial", result.getFeedback());
+    }
+
+    /**
+     * Test the result when running without a task name
+     */
+    @Test
+    public void testMissingTaskNameCommandResult() {
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.addCommand.execute("add"));
+        assertEquals("Invalid arguments.\n\n" + AddCommand.COMMAND_FORMAT, result.getFeedback());
+    }
+
+    /**
+     * Test the result when the task name consists of only empty spaces
+     */
+    @Test
+    public void testTaskNameIsEmptyCommandResult() {
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.addCommand.execute("add             "));
+        assertEquals("Invalid arguments.\n\n" + AddCommand.COMMAND_FORMAT, result.getFeedback());
     }
 }
