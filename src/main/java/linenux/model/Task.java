a0d5869899df2fc6f8aa6210cf88f692b1953c74
cadmusthefounder <cadmusthefounder@gmail.com>
2016-10-07 20:33:24 +0800

    Add boolean isDone to Task Model

--- src/main/java/linenux/model/Task.java
+++ src/main/java/linenux/model/Task.java
@@ -8,65 +8,77 @@ import java.time.LocalDateTime;
  */
 public class Task {
     private String taskName;
+    private boolean isDone;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    
-    /** 
-     * Constructor for tasks with no deadlines or predetermined time slots. 
+
+    /**
+     * Constructor for tasks with no deadlines or predetermined time slots.
      */
     public Task(String taskName) {
         this.taskName = taskName;
+        this.isDone = false;
         this.startTime = null;
         this.endTime = null;
     }
-    
-    /** 
-     * Constructor for tasks with deadlines only. 
+
+    /**
+     * Constructor for tasks with deadlines only.
      */
     public Task(String taskName, LocalDateTime endTime) {
         this.taskName = taskName;
+        this.isDone = false;
         this.startTime = null;
         this.endTime = endTime;
     }
-    
-    /** 
-     * Constructor for tasks with predetermined time slots. 
+
+    /**
+     * Constructor for tasks with predetermined time slots.
      */
     public Task(String taskName, LocalDateTime startTime, LocalDateTime endTime) {
         this.taskName = taskName;
+        this.isDone = false;
         this.startTime = startTime;
         this.endTime = endTime;
     }
-    
+
     @Override
     public String toString() {
         return taskName;
     }
-    
+
     /* Getters */
-    
+
     public String getTaskName() {
         return taskName;
     }
-    
+
+    public boolean isDone() {
+        return isDone;
+    }
+
     public LocalDateTime getStartTime() {
         return startTime;
     }
-    
+
     public LocalDateTime getEndTime() {
         return endTime;
     }
-    
-    /* Setters */    
-    
+
+    /* Setters */
+
      public void setTaskName(String taskName) {
          this.taskName = taskName;
      }
-     
+
+     public void markAsDone() {
+         this.isDone = true;
+     }
+
      public void setStartTime(LocalDateTime startTime) {
          this.startTime = startTime;
      }
-     
+
      public void setEndTime(LocalDateTime endTime) {
          this.endTime = endTime;
      }
