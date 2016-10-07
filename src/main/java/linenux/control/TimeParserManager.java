0cb4c1e7585ee2398310c8f36c8f1fcfb3c8daba
Yihang Ho <hoyihang5@gmail.com>
2016-10-07 17:53:01 +0800

    Implement TimeParserManager#canParse

--- src/main/java/linenux/control/TimeParserManager.java
+++ src/main/java/linenux/control/TimeParserManager.java
@@ -6,18 +6,33 @@ import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 /**
- * Assigns time parsers based on user input. 
+ * Assigns time parsers based on user input.
  */
 public class TimeParserManager {
     private ArrayList<TimeParser> parserList;
-    
+
     public TimeParserManager(TimeParser... parsers) {
         parserList = new ArrayList<TimeParser>();
         for (TimeParser parser: parsers){
             parserList.add(parser);
         }
     }
-    
+
+    /**
+     * Check if the manager can parse some string
+     * @param userInput The string to check
+     * @return {@code true} if and only if at least one of the parsers can parse {@code userInput}
+     */
+    public boolean canParse(String userInput) {
+        for (TimeParser parser: parserList) {
+            if (parser.respondTo(userInput)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     /**
      * Assigns the appropriate time parser to the user input.
      */
