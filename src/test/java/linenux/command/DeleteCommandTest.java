17d5419b830e5bec1e26f274f1de8e7d264d193a
Yihang Ho <hoyihang5@gmail.com>
2016-10-07 13:23:46 +0800

    Refactor tests
    Implements
    
    - assertNoChange
    - assertChangeBy

--- src/test/java/linenux/command/DeleteCommandTest.java
+++ src/test/java/linenux/command/DeleteCommandTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static linenux.helpers.Assert.assertChangeBy;
+import static linenux.helpers.Assert.assertNoChange;
 
 /**
  * Created by yihangho on 10/5/16.
@@ -47,33 +49,28 @@ public class DeleteCommandTest {
     @Test
     public void testCommandResultWhenNoMatchFound() {
         this.schedule.addTask(new Task("flkasdjfaklsdfjaldf"));
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.execute("delete that nasty todo");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.deleteCommand.execute("delete that nasty todo"));
         assertEquals("Cannot find \"that nasty todo\".", result.getFeedback());
-        assertEquals(beforeSize, afterSize);
     }
 
     @Test
     public void testCommandResultWhenOnlyOneMatchFound() {
         this.schedule.addTask(new Task("hello"));
         this.schedule.addTask(new Task("i can' type"));
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.execute("delete hello");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertChangeBy(() -> this.schedule.getTaskList().size(),
+                -1,
+                () -> this.deleteCommand.execute("delete hello"));
         assertEquals("Deleted \"hello\".", result.getFeedback());
-        assertEquals(beforeSize - 1, afterSize);
     }
 
     @Test
     public void testCommandResultWhenMultipleMatchesFound() {
         this.schedule.addTask(new Task("hello world"));
         this.schedule.addTask(new Task("say hello"));
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.execute("delete hello");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.deleteCommand.execute("delete hello"));
         assertEquals("Which one? (1-2)\n1. hello world\n2. say hello\n", result.getFeedback());
-        assertEquals(beforeSize, afterSize);
     }
 
     @Test
@@ -86,44 +83,37 @@ public class DeleteCommandTest {
     @Test
     public void testUserResponseCancel() {
         this.setupMultipleHelloTasksAndExecuteAmbiguousCommand();
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.userResponse("cancel");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.deleteCommand.userResponse("cancel"));
         assertEquals("OK! Not deleting anything.", result.getFeedback());
-        assertEquals(beforeSize, afterSize);
     }
 
     @Test
     public void testUserResponseValidIndex() {
         this.setupMultipleHelloTasksAndExecuteAmbiguousCommand();
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.userResponse("1");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertChangeBy(() -> this.schedule.getTaskList().size(),
+                -1,
+                () -> this.deleteCommand.userResponse("1"));
         assertEquals("Deleted \"hello world\".", result.getFeedback());
-        assertEquals(beforeSize - 1, afterSize);
     }
 
     @Test
     public void testUserResponseInvalidIndex() {
         this.setupMultipleHelloTasksAndExecuteAmbiguousCommand();
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.userResponse("0");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.deleteCommand.userResponse("0"));
         String expectedResponse = "That's not a valid index. Enter a number between 1 and 2:\n" +
                 "1. hello world\n2. say hello\n";
         assertEquals(expectedResponse, result.getFeedback());
-        assertEquals(beforeSize, afterSize);
     }
 
     @Test
     public void testUserResponseInvalidResponse() {
         this.setupMultipleHelloTasksAndExecuteAmbiguousCommand();
-        int beforeSize = this.schedule.getTaskList().size();
-        CommandResult result = this.deleteCommand.userResponse("roses are red");
-        int afterSize = this.schedule.getTaskList().size();
+        CommandResult result = assertNoChange(() -> this.schedule.getTaskList().size(),
+                () -> this.deleteCommand.userResponse("roses are red"));
         String expectedResponse = "I don't understand \"roses are red\".\n" +
                 "Enter a number to indicate which task to delete.\n1. hello world\n2. say hello\n";
         assertEquals(expectedResponse, result.getFeedback());
-        assertEquals(beforeSize, afterSize);
     }
 }
---
+++ src/test/java/linenux/helpers/Assert.java
@@ -0,0 +1,42 @@
+package linenux.helpers;
+
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * More assertions to make life (testing) easier.
+ *
+ * Most of these assertions can be overloaded to deal with different data types.
+ */
+public class Assert {
+    /**
+     * Assert that an integer value (supplied by {@code supplier}) does not change after performing
+     * the {@code action}.
+     * @param supplier Supplies the integer value to test.
+     * @param action The action to perform.
+     * @param <R> The return type of {@code action}.
+     * @return The return value of {@code action}. This is useful to test the return value of {@code action}.
+     */
+    public static <R> R assertNoChange(IntSupplier supplier, Supplier<R> action) {
+        return assertChangeBy(supplier, 0, action);
+    }
+
+    /**
+     * Assert that an integer value (supplied by {@code supplier}) changes by {@code delta} after performing
+     * the {@code action}. In other words, the final value should be the initial value + {@code delta}.
+     * @param supplier Supplies the integer value to test.
+     * @param delta The expected change.
+     * @param action The action to perform.
+     * @param <R> The return type of {@code action}.
+     * @return The return value of {@code action}.
+     */
+    public static <R> R assertChangeBy(IntSupplier supplier, int delta, Supplier<R> action) {
+        int before = supplier.getAsInt();
+        R output = action.get();
+        int after = supplier.getAsInt();
+        assertEquals(before + delta, after);
+        return output;
+    }
+}
