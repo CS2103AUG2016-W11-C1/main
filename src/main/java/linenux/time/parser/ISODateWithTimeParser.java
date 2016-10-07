5c63d02d1c25c3ccb288853039e86f93f8ad842f
Yihang Ho <hoyihang5@gmail.com>
2016-10-07 17:34:34 +0800

    Implement ISODateWithTimeParser

---
+++ src/main/java/linenux/time/parser/ISODateWithTimeParser.java
@@ -0,0 +1,27 @@
+package linenux.time.parser;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Parse date and time in the form of "2016-10-01 2:00PM"
+ */
+public class ISODateWithTimeParser implements TimeParser {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d h:mma");
+
+    @Override
+    public boolean respondTo(String input) {
+        try {
+            parse(input);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public LocalDateTime parse(String input) {
+        return LocalDateTime.parse(input.toUpperCase(), formatter);
+    }
+}
