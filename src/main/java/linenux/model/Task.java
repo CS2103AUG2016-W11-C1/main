333205247a121450d479031b077dd24f25f63f24
Kaiiiii <kailing_813@hotmail.com>
2016-10-15 13:38:59 +0800

    added categories to task

--- src/main/java/linenux/model/Task.java
+++ src/main/java/linenux/model/Task.java
@@ -7,24 +7,27 @@ import java.util.ArrayList;
 import linenux.util.ArrayListUtil;
 
 /**
- * Represents a task in the schedule.
- * Only taskName is a required field and cannot be an empty string.
+ * Represents a task in the schedule. Only taskName is a required field and
+ * cannot be an empty string.
  */
 public class Task {
     private String taskName;
     private boolean isDone;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+    private ArrayList<String> categories;
     private ArrayList<Reminder> reminders;
 
     /**
-     * Constructor for To-Dos (tasks with no deadlines or predetermined time slots).
+     * Constructor for To-Dos (tasks with no deadlines or predetermined time
+     * slots).
      */
     public Task(String taskName) {
         this.taskName = taskName;
         this.isDone = false;
         this.startTime = null;
         this.endTime = null;
+        this.categories = new ArrayList<String>();
         this.reminders = new ArrayList<Reminder>();
     }
 
@@ -36,6 +39,7 @@ public class Task {
         this.isDone = false;
         this.startTime = null;
         this.endTime = endTime;
+        this.categories = new ArrayList<String>();
         this.reminders = new ArrayList<Reminder>();
     }
 
@@ -47,15 +51,16 @@ public class Task {
         this.isDone = false;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.categories = new ArrayList<String>();
         this.reminders = new ArrayList<Reminder>();
     }
 
     public Task copyTask() {
         Task copyTask = new Task(taskName, startTime, endTime);
         copyTask.setIsDone(isDone);
+        copyTask.setCategories(categories);
         copyTask.setReminders(new ArrayListUtil.ChainableArrayListUtil<Reminder>(reminders)
-                                               .map(reminder -> reminder.copyReminder())
-                                               .value());
+                .map(reminder -> reminder.copyReminder()).value());
         return copyTask;
     }
 
@@ -106,33 +111,54 @@ public class Task {
         return this.endTime;
     }
 
+    public ArrayList<String> getCategories() {
+        return this.categories;
+    }
+
     public ArrayList<Reminder> getReminders() {
         return this.reminders;
     }
 
     /* Setters */
 
-     public void setTaskName(String taskName) {
-         this.taskName = taskName;
-     }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
-     public void markAsDone() {
-         this.isDone = true;
-     }
+    public void markAsDone() {
+        this.isDone = true;
+    }
 
-     public void setIsDone(boolean isDone) {
-         this.isDone = isDone;
-     }
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
 
-     public void setStartTime(LocalDateTime startTime) {
-         this.startTime = startTime;
-     }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
 
-     public void setEndTime(LocalDateTime endTime) {
-         this.endTime = endTime;
-     }
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
 
-     public void setReminders(ArrayList<Reminder> reminders) {
-         this.reminders = reminders;
-     }
+    public void setCategories(ArrayList<String> categories) {
+        this.categories = categories;
+    }
+
+    public void setReminders(ArrayList<Reminder> reminders) {
+        this.reminders = reminders;
+    }
+
+    private String categoriesToString() {
+        StringBuilder builder = new StringBuilder();
+        if (this.categories.isEmpty()) {
+            return null;
+        }
+
+        builder.append("Categories: ");
+        for (String s : this.categories) {
+            builder.append(s + " ");
+        }
+        return builder.toString();
+    }
 }
