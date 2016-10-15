89296ddc9527a6aca97a797cdf14b320c73926a1
Yihang Ho <hoyihang5@gmail.com>
2016-10-15 15:42:20 +0800

    Implement LocalDateTimeUtil

---
+++ src/main/java/linenux/util/LocalDateTimeUtil.java
@@ -0,0 +1,28 @@
+package linenux.util;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by yihangho on 10/15/16.
+ */
+public class LocalDateTimeUtil {
+    /**
+     * Return the earlier of two instances of {@code LocalDateTime}.
+     * @param a The first {@code LocalDateTime}.
+     * @param b The second {@code LocalDateTime}.
+     * @return The earlier of {@code a} and {@code b}.
+     */
+    public static LocalDateTime min(LocalDateTime a, LocalDateTime b) {
+        return a.compareTo(b) < 0 ? a : b;
+    }
+
+    /**
+     * Returns the later of two instances of {@code LocalDateTime}.
+     * @param a The first {@code LocalDateTime}.
+     * @param b The second {@code LocalDateTime}.
+     * @return The later of {@code a} and {@code b}.
+     */
+    public static LocalDateTime max(LocalDateTime a, LocalDateTime b) {
+        return a.compareTo(b) > 0 ? a : b;
+    }
+}
