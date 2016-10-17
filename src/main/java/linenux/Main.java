17b1a23de3fc15e62cb2eb4700eeaa6505b67c3e
Yihang Ho <hoyihang5@gmail.com>
2016-10-17 23:43:40 +0800

    Remove unused class

--- src/main/java/linenux/Main.java
+++ src/main/java/linenux/Main.java
@@ -1,8 +1,5 @@
 package linenux;
 
-import linenux.control.ControlUnit;
-import linenux.view.ConsoleController;
-
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -11,7 +8,6 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.fxml.FXMLLoader;
-import linenux.view.MainWindowController;
 
 /**
  * Main program for Linenux.
@@ -24,8 +20,6 @@ public class Main extends Application implements Stoppable {
     public static final int INITIAL_CONSOLE_WIDTH = 800;
     public static final int INITIAL_CONSOLE_HEIGHT = 500;
 
-    private ConsoleController consoleController;
-
     public static void main(String args) {
         launch(args);
     }
@@ -33,15 +27,7 @@ public class Main extends Application implements Stoppable {
     // TODO: Handle general exception & make TextArea keyboard insensitive.
     @Override
     public void start(Stage primaryStage) throws Exception {
-        try {
-//            consoleController = setUpConsole(primaryStage);
-//            consoleController.setControlUnit(new ControlUnit());
-//            consoleController.setActiveControl();
-//            consoleController.displayWelcomeMessage(VERSION);
-            setupMainWindow(primaryStage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        setupMainWindow(primaryStage);
     }
 
     @Override
@@ -51,17 +37,7 @@ public class Main extends Application implements Stoppable {
         System.exit(0);
     }
 
-    private ConsoleController setUpConsole(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("/view/Console.fxml"));
-        primaryStage.setTitle(APP_NAME);
-        primaryStage.getIcons().add(new Image("/images/terminal.png"));
-        primaryStage.setScene(new Scene(loader.load(), INITIAL_CONSOLE_WIDTH, INITIAL_CONSOLE_HEIGHT));
-        primaryStage.show();
-        return loader.getController();
-    }
-
-    private MainWindowController setupMainWindow(Stage primaryStage) {
+    private void setupMainWindow(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("/view/MainWindow.fxml"));
@@ -70,11 +46,8 @@ public class Main extends Application implements Stoppable {
             Scene scene = new Scene(loader.load(), INITIAL_CONSOLE_WIDTH, INITIAL_CONSOLE_HEIGHT);
             primaryStage.setScene(scene);
             primaryStage.show();
-            MainWindowController controller = loader.getController();
-            return controller;
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
     }
 }
