46b9c823238ce34d277193934419d8faeb4a3430
Yihang Ho <hoyihang5@gmail.com>
2016-10-24 21:37:30 +0800

    Implement Either#bind

--- src/test/java/linenux/util/EitherTest.java
+++ src/test/java/linenux/util/EitherTest.java
@@ -3,6 +3,7 @@ package linenux.util;
 import org.junit.Test;
 
 import java.util.NoSuchElementException;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -36,5 +37,48 @@ public class EitherTest {
     @Test(expected=NoSuchElementException.class)
     public void testGetRightShouldThrow() {
         Either.left("hello").getRight();
+    }
+
+    @Test
+    public void testIsLeftBindReturnsLeft() {
+        Either<Integer, Integer> either = Either.left(1);
+        Either<Integer, Integer> result = either.bind(i -> Either.left(i + 1));
+
+        assertTrue(result.isLeft());
+        assertEquals(2, (int)result.getLeft());
+    }
+
+    @Test
+    public void testIsLeftBindReturnsRight() {
+        Either<Integer, Integer> either = Either.left(1);
+        Either<Integer, Integer> result = either.bind(i -> Either.right(i + 1));
+
+        assertTrue(result.isRight());
+        assertEquals(2, (int)result.getRight());
+    }
+
+    private class MockLambda implements Function<Integer, Either<Integer, Integer>> {
+        private boolean executed = false;
+
+        @Override
+        public Either<Integer, Integer> apply(Integer integer) {
+            this.executed = true;
+            return Either.left(integer + 1);
+        }
+
+        public boolean isExecuted() {
+            return this.executed;
+        }
+    }
+
+    @Test
+    public void testIsRightBind() {
+        MockLambda fn = new MockLambda();
+        Either<Integer, Integer> either = Either.right(1);
+        Either<Integer, Integer> result = either.bind(fn);
+
+        assertFalse(fn.isExecuted());
+        assertTrue(result.isRight());
+        assertEquals(1, (int)result.getRight());
     }
 }
