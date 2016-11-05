8de9a0185158521ff45c8d94619dd790ce868a44
Yihang Ho <hoyihang5@gmail.com>
2016-11-05 14:05:01 +0800

    Improve SaveCommand

--- src/main/java/linenux/command/SaveCommand.java
+++ src/main/java/linenux/command/SaveCommand.java
@@ -1,10 +1,15 @@
 package linenux.command;
 
+import linenux.command.result.CommandResult;
+import linenux.control.ControlUnit;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import linenux.command.result.CommandResult;
-import linenux.control.ControlUnit;
 
 //@@author A0144915A
 public class SaveCommand extends AbstractCommand {
@@ -13,23 +18,66 @@ public class SaveCommand extends AbstractCommand {
     private static final String COMMAND_FORMAT = "save PATH";
 
     private ControlUnit controlUnit;
+    private Path basePath;
+    private boolean requiresUserResponse = false;
+    private String pendingSavePath = null;
 
     public SaveCommand(ControlUnit controlUnit) {
+        this(controlUnit, Paths.get("").toAbsolutePath());
+    }
+
+    public SaveCommand(ControlUnit controlUnit, Path basePath) {
         this.controlUnit = controlUnit;
+        this.basePath = basePath;
+        this.TRIGGER_WORDS.add(TRIGGER_WORD);
     }
 
     @Override
     public CommandResult execute(String userInput) {
-        String path = this.extractArgument(userInput);
+        String relativePath = this.extractArgument(userInput);
 
-        if (path.isEmpty()) {
+        if (relativePath.isEmpty()) {
             return this.makeInvalidArgumentResult();
         }
 
-        this.controlUnit.saveSchedule();
-        this.controlUnit.setScheduleFilePath(path);
+        Path targetPath = this.basePath.resolve(relativePath);
+        Path parentPath = targetPath.getParent();
+        String path = this.basePath.resolve(relativePath).toString();
+        File file = new File(path);
 
-        return makeResult(path);
+        try {
+            new File(parentPath.toString()).mkdirs();
+
+            if (file.exists()) {
+                this.requiresUserResponse = true;
+                this.pendingSavePath = path;
+                return this.makeFileExistsResult(path);
+            } else {
+                new FileOutputStream(file).close();
+            }
+        } catch (Exception e) {
+        }
+
+        return this.validateAndUpdate(file);
+    }
+
+    @Override
+    public boolean awaitingUserResponse() {
+        return this.requiresUserResponse;
+    }
+
+    @Override
+    public CommandResult getUserResponse(String userInput) {
+        if (userInput.trim().toLowerCase().equals("yes")) {
+            this.requiresUserResponse = false;
+            File file = new File(this.pendingSavePath);
+            return this.validateAndUpdate(file);
+        } else if (userInput.trim().toLowerCase().equals("no")) {
+            this.requiresUserResponse = false;
+            return this.makeCancelResult();
+        } else {
+            return this.makeInvalidUserResponse();
+        }
     }
 
     @Override
@@ -57,11 +105,38 @@ public class SaveCommand extends AbstractCommand {
         }
     }
 
+    private CommandResult validateAndUpdate(File file) {
+        if (file.canWrite()) {
+            this.controlUnit.setScheduleFilePath(file.toString());
+            this.controlUnit.saveSchedule();
+            return makeResult(file.toString());
+        } else {
+            return makeNotWritableResult(file.toString());
+        }
+    }
+
     private CommandResult makeResult(String path) {
         return () -> "Saved to " + path;
     }
 
+    private CommandResult makeNotWritableResult(String path) {
+        return () -> "Cannot save to " + path;
+    }
+
+    private CommandResult makeFileExistsResult(String path) {
+        return () -> path + " already exists.\n" +
+                "Do you want to overwrite it? (yes/no)";
+    }
+
+    private CommandResult makeCancelResult() {
+        return () -> "OK! Not overwriting " + this.pendingSavePath;
+    }
+
     private CommandResult makeInvalidArgumentResult() {
         return () -> "Invalid arguments.\n\n" + COMMAND_FORMAT + "\n\n" + CALLOUTS;
+    }
+
+    private CommandResult makeInvalidUserResponse() {
+        return () -> "I don't understand that. Do you want to overwrite " + this.pendingSavePath + "? (yes/no)";
     }
 }
---
+++ src/test/java/linenux/command/SaveCommandTest.java
@@ -0,0 +1,210 @@
+package linenux.command;
+
+import linenux.command.result.CommandResult;
+import linenux.config.Config;
+import linenux.control.ControlUnit;
+import linenux.model.Schedule;
+import linenux.storage.ScheduleStorage;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+//@@author A0144915A
+public class SaveCommandTest {
+    private Path tempDir;
+    private ScheduleStorage storage;
+    private Config config;
+    private ControlUnit controlUnit;
+    private SaveCommand saveCommand;
+
+    @Before
+    public void setupTestEnvironment() throws Exception {
+        this.tempDir = Files.createTempDirectory("tmp").toAbsolutePath();
+        this.storage = new MockStorage();
+        this.config = new MockConfig();
+        this.controlUnit = new ControlUnit(this.storage, this.config, null);
+        this.saveCommand = new SaveCommand(this.controlUnit, this.tempDir);
+    }
+
+    @Test
+    public void testWritableNewFileByAbsolutePath() throws Exception {
+        // Save to a writable, but non-existent file by absolute path.
+        // Config and storage should be updated.
+        Path anotherTempDir = Files.createTempDirectory("tmp").toAbsolutePath();
+        Path targetPath = anotherTempDir.resolve("schedule.xml");
+        String target = targetPath.toString();
+
+        CommandResult result = this.saveCommand.execute("save " + target);
+        String expectedResult = "Saved to " + target;
+        assertEquals(expectedResult, result.getFeedback());
+        assertEquals(target, this.config.getScheduleFilePath());
+    }
+
+    @Test
+    public void testWritableNewFileByRelativePath() {
+        // Save to a writable, but non-existent file by relative path.
+        // Config and storage should be updated with the absolute path.
+        CommandResult result = this.saveCommand.execute("save hello.xml");
+        String expectedPath = this.tempDir.resolve("hello.xml").toString();
+        String expectedResult = "Saved to " + expectedPath;
+        assertEquals(expectedResult, result.getFeedback());
+        assertEquals(expectedPath, this.config.getScheduleFilePath());
+    }
+
+    @Test
+    public void testWritableExistingFile() throws Exception {
+        // Save to a writable, but existing file.
+        // Should prompt the user.
+        String target = this.tempDir.resolve("hello.xml").toString();
+        new FileOutputStream(target).close();
+        CommandResult result = this.saveCommand.execute("save hello.xml");
+        String expectedResult = target + " already exists.\n" +
+                "Do you want to overwrite it? (yes/no)";
+        assertEquals(expectedResult, result.getFeedback());
+        assertEquals("existingPath", this.config.getScheduleFilePath());
+        assertTrue(this.saveCommand.awaitingUserResponse());
+
+        result = this.saveCommand.getUserResponse("bla");
+        expectedResult = "I don't understand that. Do you want to overwrite " + target + "? (yes/no)";
+        assertEquals(expectedResult, result.getFeedback());
+        assertEquals("existingPath", this.config.getScheduleFilePath());
+        assertTrue(this.saveCommand.awaitingUserResponse());
+
+        result = this.saveCommand.getUserResponse("no");
+        expectedResult = "OK! Not overwriting " + target;
+        assertEquals(expectedResult, result.getFeedback());
+        assertEquals("existingPath", this.config.getScheduleFilePath());
+        assertFalse(this.saveCommand.awaitingUserResponse());
+
+        this.saveCommand.execute("save hello.xml");
+        assertTrue(this.saveCommand.awaitingUserResponse());
+        result = this.saveCommand.getUserResponse("yes");
+        expectedResult = "Saved to " + target;
+        assertEquals(expectedResult, result.getFeedback());
+        assertEquals(target, this.config.getScheduleFilePath());
+        assertFalse(this.saveCommand.awaitingUserResponse());
+    }
+
+    @Test
+    public void testNonWritableFile() throws Exception {
+        // Save to a non-writable file.
+        // Should return the correct command result.
+        // Config and storage should not be updated.
+        String target = this.tempDir.resolve("notWritable.xml").toString();
+        File f = new File(target);
+        new FileOutputStream(f).close();
+        f.setWritable(false);
+
+        CommandResult result = this.saveCommand.execute("save notWritable.xml");
+        String expectedFeedback = target + " already exists.\n" +
+                "Do you want to overwrite it? (yes/no)";
+        assertEquals(expectedFeedback, result.getFeedback());
+        assertEquals("existingPath", this.config.getScheduleFilePath());
+
+        result = this.saveCommand.getUserResponse("yes");
+        expectedFeedback = "Cannot save to " + target;
+        assertEquals(expectedFeedback, result.getFeedback());
+        assertEquals("existingPath", this.config.getScheduleFilePath());
+    }
+
+    @Test
+    public void testNonWritableParent() {
+        File f = new File(this.tempDir.toString());
+        f.setWritable(false);
+
+        CommandResult result = this.saveCommand.execute("save notWritable.xml");
+        String expectedPath = this.tempDir.resolve("notWritable.xml").toString();
+        String expectedFeedback = "Cannot save to " + expectedPath;
+        assertEquals(expectedFeedback, result.getFeedback());
+        assertEquals("existingPath", this.config.getScheduleFilePath());
+    }
+
+    @Test
+    public void testNonExistentParentFolder() {
+        CommandResult result = this.saveCommand.execute("save subdir/schedule.xml");
+
+        Path parent = this.tempDir.resolve("subdir");
+        File parentFile = new File(parent.toString());
+        assertTrue(parentFile.exists());
+        assertTrue(parentFile.isDirectory());
+
+        Path target = parent.resolve("schedule.xml");
+        File targetFile = new File(target.toString());
+        assertTrue(targetFile.exists());
+        assertTrue(targetFile.isFile());
+
+        String expectedPath = target.toString();
+        String expectedFeedback = "Saved to " + expectedPath;
+        assertEquals(expectedFeedback, result.getFeedback());
+        assertEquals(expectedPath, this.config.getScheduleFilePath());
+    }
+
+    @Test
+    public void testInvalidArgument() {
+        CommandResult result = this.saveCommand.execute("save");
+        String expectedFeedback = "Invalid arguments.\n\n" +
+                "save PATH\n\n" +
+                "* Non-compulsory fields are in square brackets.\n" +
+                "* Arguments are case insensitive.";
+        assertEquals(expectedFeedback, result.getFeedback());
+    }
+
+    private static class MockStorage implements ScheduleStorage {
+        @Override
+        public Schedule loadScheduleFromFile() {
+            return null;
+        }
+
+        @Override
+        public void saveScheduleToFile(Schedule schedule) {
+        }
+
+        @Override
+        public boolean hasScheduleFile() {
+            return true;
+        }
+    }
+
+    private static class MockConfig implements Config {
+        private String scheduleFilePath = "existingPath";
+
+        @Override
+        public String getVersionNo() {
+            return "test";
+        }
+
+        @Override
+        public String getScheduleFilePath() {
+            return this.scheduleFilePath;
+        }
+
+        @Override
+        public void setScheduleFilePath(String path) {
+            this.scheduleFilePath = path;
+        }
+
+        @Override
+        public boolean hasConfigFile() {
+            return true;
+        }
+
+        @Override
+        public Collection<String> getAliases(String triggerWords) {
+            return null;
+        }
+
+        @Override
+        public void setAliases(String triggerWord, Collection<String> aliases) {
+
+        }
+    }
+}
